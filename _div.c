56d4f7bde2d52e5db641d69b26e2fc0cbed08ee6
Lucieruin <Lucie.wattez@hotmail.fr>
2023-09-07 05:30:30 -0700

    add condition division by zero for return error

--- _div.c
+++ _div.c
@@ -15,6 +15,11 @@ void _div(stack_t **stack, unsigned int lineNumber)
 		fprintf(stderr, "L%d: can't div, stack too short\n", lineNumber);
 		exit(EXIT_FAILURE);
 	}
+	if (current->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", lineNumber);
+		exit(EXIT_FAILURE);
+	}
 	
 	current->next->n /= current->n;
 	_pop(stack, lineNumber);
